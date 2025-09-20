9d42e77370a4ab288f4fe0e05dee5cbd9c1b050a
samuel <samuel86102@gmail.com>
2025-09-20 23:21:04 +0800

    change to fix tables

--- app.py
+++ app.py
@@ -4,10 +4,21 @@ eventlet.monkey_patch()
 
 from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
+import random
 
 # --- 配置 ---
-TOTAL_TABLES = 3  # 可在此調整總桌數
-NUMBER_RANGE = (1, 10) # 數字範圍
+# 根據 layout.jpg 的固定桌位定義
+TABLE_DEFINITIONS = [
+    {'id': 'D6', 'name': '體育', 'group': 'de'}, {'id': 'E6', 'name': '體育', 'group': 'de'}, {'id': 'F6', 'name': '國文', 'group': 'fgi'}, {'id': 'G6', 'name': '國文', 'group': 'fgi'}, {'id': 'H6', 'name': '國文', 'group': 'fgi'}, {'id': 'I6', 'name': '國文', 'group': 'fgi'},
+    {'id': 'A5', 'name': '音樂', 'group': 'abc'}, {'id': 'B5', 'name': '美術', 'group': 'abc'}, {'id': 'C5', 'name': '美術', 'group': 'abc'}, {'id': 'D5', 'name': '體育', 'group': 'de'}, {'id': 'E5', 'name': '工教', 'group': 'de'}, {'id': 'F5', 'name': '英語', 'group': 'fgi'}, {'id': 'G5', 'name': '英語', 'group': 'fgi'}, {'id': 'H5', 'name': '英語', 'group': 'fgi'}, {'id': 'I5', 'name': '家政', 'group': 'fgi'}, {'id': 'J5', 'name': '公訓', 'group': 'hj'},
+    {'id': 'A4', 'name': '音樂', 'group': 'abc'}, {'id': 'B4', 'name': '美術', 'group': 'abc'}, {'id': 'C4', 'name': '美術', 'group': 'abc'}, {'id': 'D4', 'name': '體育', 'group': 'de'}, {'id': 'E4', 'name': '工教', 'group': 'de'}, {'id': 'F4', 'name': '英語', 'group': 'fgi'}, {'id': 'G4', 'name': '英語', 'group': 'fgi'}, {'id': 'H4', 'name': '教心', 'group': 'hj'}, {'id': 'I4', 'name': '教心', 'group': 'hj'}, {'id': 'J4', 'name': '公訓', 'group': 'hj'},
+    {'id': 'A3', 'name': '化學', 'group': 'abc'}, {'id': 'B3', 'name': '化學', 'group': 'abc'}, {'id': 'C3', 'name': '化學', 'group': 'abc'}, {'id': 'D3', 'name': '物理、自然科學研究所', 'group': 'de'}, {'id': 'E3', 'name': '54體育、工教化學、物應', 'group': 'de'}, {'id': 'F3', 'name': '英語', 'group': 'fgi'}, {'id': 'G3', 'name': '英語', 'group': 'fgi'}, {'id': 'H3', 'name': '教心', 'group': 'hj'}, {'id': 'I3', 'name': '教心', 'group': 'hj'}, {'id': 'J3', 'name': '社會', 'group': 'hj'},
+    {'id': 'A2', 'name': '生物', 'group': 'abc'}, {'id': 'B2', 'name': '生物', 'group': 'abc'}, {'id': 'C2', 'name': '數學', 'group': 'abc'}, {'id': 'D2', 'name': '54數學', 'group': 'de'}, {'id': 'E2', 'name': '54數學', 'group': 'de'}, {'id': 'F2', 'name': '54英語地理', 'group': 'fgi'}, {'id': 'G2', 'name': '地理', 'group': 'fgi'}, {'id': 'H2', 'name': '教育', 'group': 'hj'}, {'id': 'I2', 'name': '教育', 'group': 'hj'}, {'id': 'J2', 'name': '衛教', 'group': 'hj'},
+    {'id': 'A1', 'name': '生物', 'group': 'abc'}, {'id': 'B1', 'name': '數學', 'group': 'abc'}, {'id': 'C1', 'name': '數學', 'group': 'abc'}, {'id': 'D1', 'name': '54數學', 'group': 'de'},
+    {'id': 'G1', 'name': '歷史', 'group': 'fgi'}, {'id': 'H1', 'name': '教育', 'group': 'hj'}, {'id': 'I1', 'name': '教育', 'group': 'hj'}, {'id': 'J1', 'name': '衛教', 'group': 'hj'},
+    {'id': '主桌', 'name': '主桌', 'group': 'main'},
+]
+NUMBER_RANGE = (1, 100) # 數字範圍
 
 # --- 應用程式初始化 ---
 app = Flask(__name__)
@@ -15,10 +26,9 @@ app.config['SECRET_KEY'] = 'a_very_secret_key' # 正式環境請更換為更複�
 socketio = SocketIO(app, async_mode='eventlet')
 
 # --- 遊戲狀態管理 ---
-# 使用字典來儲存每桌的狀態
-# 結構: {1: {'number': None, 'locked': False, 'sid': None}, 2: {...}, ...}
+# 結構: {'A5': {'number': None, 'locked': False, 'sid': None}, 'B5': {...}, ...}
 tables_state = {
-    i: {'number': None, 'locked': False, 'sid': None} for i in range(1, TOTAL_TABLES + 1)
+    table['id']: {'number': None, 'locked': False, 'sid': None} for table in TABLE_DEFINITIONS
 }
 # 追蹤已經被選走的數字
 used_numbers = set()
@@ -27,7 +37,7 @@ def get_game_state():
     """返回整個遊戲的狀態"""
     return {
         'tables': tables_state,
-        'total_tables': TOTAL_TABLES,
+        'layout': TABLE_DEFINITIONS,
         'all_locked': all(table['locked'] for table in tables_state.values()),
         'number_range': NUMBER_RANGE
     }
@@ -71,8 +81,12 @@ def handle_disconnect():
     # 檢查是否有桌子被這個離線的客戶端佔用，若有則釋放
     for table_id, data in tables_state.items():
         if data['sid'] == request.sid:
-            data['sid'] = None
-            print(f"Table {table_id} has been released.")
+            # 將該桌的號碼從 used_numbers 中移除
+            if data['locked'] and data['number'] in used_numbers:
+                used_numbers.remove(data['number'])
+            # 重置該桌狀態
+            tables_state[table_id] = {'number': None, 'locked': False, 'sid': None}
+            print(f"Table {table_id} has been released due to disconnect.")
             # 廣播更新後的狀態
             socketio.emit('update_state', get_game_state())
             break
@@ -80,7 +94,7 @@ def handle_disconnect():
 @socketio.on('select_table')
 def handle_select_table(data):
     """當使用者在手機上選擇桌號時"""
-    table_id = int(data['table_id'])
+    table_id = data['table_id']
 
     # 檢查該桌是否已被佔用
     if tables_state[table_id]['sid'] is not None and tables_state[table_id]['sid'] != request.sid:
@@ -105,19 +119,17 @@ def handle_select_table(data):
 @socketio.on('stop_number')
 def handle_stop_number(data):
     """當使用者按下按鈕鎖定數字時"""
-    table_id = int(data['table_id'])
+    table_id = data['table_id']
     
     # 權限檢查：只有佔用該桌的 session ID 才能鎖定數字
-    if tables_state[table_id]['sid'] != request.sid:
+    if tables_state.get(table_id, {}).get('sid') != request.sid:
         print(f"Unauthorized attempt to stop table {table_id} by {request.sid}")
         return
 
     if not tables_state[table_id]['locked']:
-        import random
         # 產生一個獨一無二的隨機數
         available_numbers = list(set(range(NUMBER_RANGE[0], NUMBER_RANGE[1] + 1)) - used_numbers)
         if not available_numbers:
-            # 如果沒有可用的數字了（理論上桌數小於等於數字數量時不會發生）
             print("Error: No available numbers left!")
             return
 
@@ -147,7 +159,7 @@ def handle_reset_game():
     """處理管理員重置遊戲的請求"""
     global tables_state, used_numbers
     tables_state = {
-        i: {'number': None, 'locked': False, 'sid': None} for i in range(1, TOTAL_TABLES + 1)
+        table['id']: {'number': None, 'locked': False, 'sid': None} for table in TABLE_DEFINITIONS
     }
     used_numbers = set()
     print("--- GAME RESET by admin ---")
@@ -160,7 +172,7 @@ def handle_reset_game():
 @socketio.on('reset_table')
 def handle_reset_table(data):
     """處理管理員重置單一桌次的請求"""
-    table_id = int(data['table_id'])
+    table_id = data['table_id']
 
     if table_id in tables_state:
         table_to_reset = tables_state[table_id]
@@ -183,18 +195,6 @@ def handle_reset_table(data):
 
         # 廣播更新後的狀態
         socketio.emit('update_state', get_game_state())
-
-
-@socketio.on('set_total_tables')
-def handle_set_total_tables(data):
-    """設定總桌數並重置遊戲"""
-    global TOTAL_TABLES
-    new_total = int(data.get('total_tables'))
-    if new_total and new_total > 0:
-        TOTAL_TABLES = new_total
-        print(f"--- Admin set total tables to {TOTAL_TABLES} ---")
-        # 重置整個遊戲
-        handle_reset_game()
 
 
 @socketio.on('set_number_range')
